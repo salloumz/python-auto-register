b78b03470fb77b11261c4b48c77332ab3dcdb68c
dylankrish <91637207+dylankrish@users.noreply.github.com>
2022-11-11 00:54:18 -0500

    yes

--- autoregister.py
+++ autoregister.py
@@ -8,12 +8,14 @@ chromeoptions = webdriver.ChromeOptions()
 
 # MacOS
 chromedriver = "/opt/homebrew/bin/chromedriver"
-chromeoptions.binary_location = "/Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser"
+chromeoptions.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
 chromeoptions.add_argument("--user-data-dir=/Users/dylank/Library/Application Support/BraveSoftware/Brave-Browser/Default")
 
 
-driver = webdriver.Chrome(service=Service(chromedriver))
+driver = webdriver.Chrome(service=Service(chromedriver), options=chromeoptions)
 
 driver.get("brave://newtab")
 
+time.wait(15)
 
+driver.close
