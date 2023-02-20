6dcb79fce31b5c5eaef8a184fdd0fc5c69a5e457
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-02-20 14:13:21 -0500

    TODO: Add support for other browsers

--- autoregister.py
+++ autoregister.py
@@ -14,6 +14,7 @@ from selenium.webdriver.chrome.service import Service
 chromeoptions = webdriver.ChromeOptions()
 
 # TODO: Add support for Windows
+# TODO: Add support for other browsers
 if platform == "linux" or platform == "linux2":
     # Linux
     print('Linux')
