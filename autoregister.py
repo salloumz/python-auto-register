ab7c30bc619428543e4c82cd39b3129b97da6642
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-02-20 14:14:06 -0500

    TODO: Clean up code comments

--- autoregister.py
+++ autoregister.py
@@ -1,4 +1,5 @@
 # TODO: Create functions for each step and allow disabling of certain steps
+# TODO: Clean up code comments
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
