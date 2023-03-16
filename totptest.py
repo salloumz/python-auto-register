6b3c115bf77b4287f6ee22e860fa8dbcc118b09b
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-03-16 17:30:22 -0400

    TOTP Code to prepare for transition to MS MFA

--- totptest.py
+++ totptest.py
@@ -1,3 +1,12 @@
+import hashlib
 import pyotp
 
-# TODO: add microsoft 2fa support
+def generate_2fa_code(secret):
+    # Create a TOTP object with the given secret
+    totp = pyotp.TOTP(secret, digits=6, hashfunc=hashlib.sha1)
+
+    # Get the current TOTP code
+    code = totp.now()
+
+    # Return the TOTP code as a string
+    return str(code)
