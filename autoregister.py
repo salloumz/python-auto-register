ef55b8e4fe817b49c1d1ce673a26dc4eb57e575c
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-03-17 17:27:23 -0400

    Enrollment Page

--- autoregister.py
+++ autoregister.py
@@ -106,7 +106,11 @@ while not enrollmentBtnFound:
 
 # hit the enrollment button
 
-driver.execute_script("submitAction_win0(document.win0,'PE_UI020_BTNS_PE_GRID_BUTTON$1')")
+# driver.execute_script("submitAction_win0(document.win0,'PE_UI020_BTNS_PE_GRID_BUTTON$1')")
+
+# go to the enrolllment page
+
+driver.get('https://www.lionpath.psu.edu/psc/CSPRD/EMPLOYEE/SA/c/NUI_FRAMEWORK.PT_AGSTARTPAGE_NUI.GBL?CONTEXTIDPARAMS=TEMPLATE_ID%3aPTPPNAVCOL&scname=PE_PT_NVF_ENROLLMENT&PanelCollapsible=Y&PTPPB_GROUPLET_ID=PE_PT_NVI_ENROLLMENT&CRefName=PE_PT_NVI_ENROLLMENT&AJAXTransfer=y')
 
 # Wait until next page is loaded, specifically the shopping cart button
 # WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="win1divPTGP_STEP_DVW_PTGP_STEP_BTN_GB$4"]')))
