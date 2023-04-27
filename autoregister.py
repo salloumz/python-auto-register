1eaaf0e66f63d1c0f5b457e8381486ca4b75a691
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-04-27 15:45:46 -0400

    Completed TODO

--- autoregister.py
+++ autoregister.py
@@ -1,5 +1,4 @@
 # TODO: Create functions for each step and allow disabling of certain steps
-# TODO: Clean up code comments
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
