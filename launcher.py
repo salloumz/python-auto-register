30e73e08f819f6a2c0ac42960c9e746bfd19ae36
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-04-27 16:26:59 -0400

    Start launcher

---
+++ launcher.py
@@ -0,0 +1,6 @@
+import subprocess
+import time
+import datetime
+
+while datetime.datetime.now().hour == 23 or datetime.datetime.now().minute == 58:
+    subprocess.call(['python3', 'autoregister.py'])
