c6466583a7d0849718a97d4f218bdd13b785ffe4
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-04-29 15:06:51 -0400

    this should be an 'and' statement

--- launcher.py
+++ launcher.py
@@ -2,5 +2,5 @@ import subprocess
 import time
 import datetime
 
-while datetime.datetime.now().hour == 23 or datetime.datetime.now().minute == 58:
+while datetime.datetime.now().hour == 23 and datetime.datetime.now().minute == 58:
     subprocess.call(['python3', 'autoregister.py'])
