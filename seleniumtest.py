3a5224fcf33006f20f96cbd5512dadd4565d04a5
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-05-02 17:07:20 -0400

    Copilot Test

--- seleniumtest.py
+++ seleniumtest.py
@@ -3,28 +3,53 @@ from selenium import webdriver
 
 from selenium.webdriver.chrome.service import Service
 
+# Set the path to the chromedriver executable
 chromedriver = "/usr/bin/chromedriver"
 
+# Set the path to the Brave executable
+brave_location = "/usr/lib/brave-bin/brave"
+
+# Set the path to the Brave profile
+brave_profile = "/home/dylank/.config/BraveSoftware/Brave-Browser"
+
+# Create a ChromeOptions object
 option = webdriver.ChromeOptions()
 
-option.binary_location = '/usr/lib/brave-bin/brave'
+# Set the binary location
+option.binary_location = brave_location
 
-option.add_argument("--user-data-dir=/home/dylank/.config/BraveSoftware/Brave-Browser")
+# Set the user data directory
+option.add_argument(f"--user-data-dir={brave_profile}")
+
+# Enable hardware acceleration
 option.add_argument("--enable-features=VaapiVideoEncoder,VaapiVideoDecoder")
+
+# Enable GPU rasterization
 option.add_argument("--enable-gpu-rasterization")
 
+# Create a Service object
 s = Service(chromedriver)
 
+# Create a ChromeDriver object
 driver = webdriver.Chrome(service=s, options=option)
 
+# Open a new tab
 driver.get("chrome://newtab")
 
-x = 1
+# Create a variable to hold the loop state
+running = True
 
-while x == 1:
+# Run the loop until the user enters 'stop'
+while running:
+    # Ask the user for input
     val = input("Enter your value: ")
-    print(val)
-    print(x)
-    x = val
 
+    # Print the user's input
+    print(val)
+
+    # If the user entered 'stop', stop the loop
+    if val == "stop":
+        running = False
+
+# Close the browser
 driver.close()
