57a959866dc6548382769e5b47a92a7f859330ed
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-05-09 16:11:04 -0400

    Add config options

---
+++ config.py
@@ -0,0 +1,13 @@
+# Config Options
+# How many classes are you enrolling in?
+# TODO: we can auto detect this by counting the number of checkboxes
+enrollnum = 2
+# Should we wait until 12AM to enroll?
+waitUntil12AM = False
+# Experimental // Not fully implemented
+# Is TOTP enabled for this Microsoft account? If so, it can be automated in the script
+useTOTP = True
+# Should we send a notification in Discord with results/if a class fails to enroll?
+sendDiscordNotification = False
+# Should we send an email with results/if a class fails to enroll?
+sendEmailNotification = False
