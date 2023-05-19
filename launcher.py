d439d70aa70f4d74d54ec203800f6721ade37826
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-05-18 22:39:03 -0400

    Add Automatic Error Handler

--- launcher.py
+++ launcher.py
@@ -2,5 +2,14 @@ import subprocess
 import time
 import datetime
 
+def run():
+    try:
+        subprocess.call(['python3', 'autoregister.py'])
+    except Exception as e:
+        print(e)
+        run()
+    finally:
+        print('Execution completed.')
+
 while datetime.datetime.now().hour == 23 and datetime.datetime.now().minute == 58:
-    subprocess.call(['python3', 'autoregister.py'])
+    run()
